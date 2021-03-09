751c6a6c5406d1424bc2f016c6b47a732ac27fc2
lodwinmoloto <lodwin@anylytical.co.za>
2021-03-09 18:11:44 +0200

    CP - WIP - Application Setup.

---
+++ src/main/java/com/ent/cross/platform/CrossPlatformApplication.java
@@ -0,0 +1,16 @@
+package com.ent.cross.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossPlatformApplication {
+
+    /**
+     * Entry point into the cross platform application.
+     * @param args
+     */
+    public static void main(String[] args){
+        SpringApplication.run(CrossPlatformApplication.class, args);
+    }
+}
